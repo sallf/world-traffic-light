46c499176ee08e60034be1bf43e81989a406399d
Saylor Allf <online@sallf.com>
2024-04-19 19:02:56 -0600

    add provider

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
+import { AppStore, makeStore } from '@world-traffic-light/redux';
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
-import { makeStore, AppStore } from '../lib/store';
 
 export default function StoreProvider({
   children,
