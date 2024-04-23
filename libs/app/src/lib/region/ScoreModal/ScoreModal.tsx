f4a7a427d4fc2e8575432221688ffc885a9ba82c
Saylor Allf <online@sallf.com>
2024-04-23 14:56:21 -0600

    dropdown for product

--- libs/app/src/lib/region/ScoreModal/ScoreModal.tsx
+++ libs/app/src/lib/region/ScoreModal/ScoreModal.tsx
@@ -1,6 +1,7 @@
 import { Close } from '@mui/icons-material'
 import { Country } from '@world-traffic-light/utils'
 import { Gauge } from '../../typography'
+import { ProductSelect } from './ProductSelect'
 
 interface Props {
   selectedCountry: Country | null
@@ -21,9 +22,12 @@ export const ScoreModal = (props: Props) => {
       className={`bg-slate-50 relative rounded-md p-10 pl-12 h-full overflow-y-auto w-[38rem] max-w-full`}
     >
       <div className="flex justify-between">
-        <p className="text-3xl">{selectedCountry?.name}</p>
+        <div>
+          <p className="text-3xl">{selectedCountry?.name}</p>
+          <ProductSelect />
+        </div>
         <div className="w-32">
-          <Gauge score={50} />
+          <Gauge score={50.123} />
         </div>
       </div>
       <button
--- libs/app/src/lib/typography/Gauge/Gauge.tsx
+++ libs/app/src/lib/typography/Gauge/Gauge.tsx
@@ -15,7 +15,7 @@ export const Gauge = (props: Props) => {
   // ---------------------
   return (
     <div className="text-center">
-      <p className="font-bold text-5xl mb-2">{score}</p>
+      <p className="font-bold text-5xl mb-2">{score.toFixed(2)}</p>
       <Slider score={score} />
     </div>
   )
